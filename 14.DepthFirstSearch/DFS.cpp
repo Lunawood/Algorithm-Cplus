a7465e2f8e60be295e93311312029af8410b971f
Lunawood <whail14@naver.com>
2022-09-22 22:51:02 +0900

    DFS

---
+++ 14.DepthFirstSearch/DFS.cpp
@@ -0,0 +1,55 @@
+#include<iostream>
+#include<vector>
+using namespace std;
+
+// DFS ±íÀÌ ¿ì¼± Å½»ö 
+int number = 7;
+int c[7];
+vector<int> a[8];
+
+void dfs(int x) {
+	if(c[x]) return;
+	c[x] = true;
+	cout << x << ' ';
+	for(int i = 0; i < a[x].size(); i++){
+		int y = a[x][i];
+		dfs(y);
+	}
+}
+
+void element() {
+	a[1].push_back(2);
+	a[2].push_back(1);
+	
+	a[1].push_back(3);
+	a[3].push_back(1);
+	
+	a[2].push_back(3);
+	a[3].push_back(2);
+	
+	a[2].push_back(4);
+	a[4].push_back(2);
+	
+	a[2].push_back(5);
+	a[5].push_back(2);
+	
+	a[4].push_back(5);
+	a[5].push_back(4);
+	
+	a[3].push_back(6);
+	a[6].push_back(3);
+	
+	a[3].push_back(7);
+	a[7].push_back(3);
+	
+	a[6].push_back(7);
+	a[7].push_back(6);
+}
+
+int main(void){
+	element();
+	
+	dfs(1);
+	
+	return 0;	
+}
