15027af37712e8137e35746d5b4f73af84999f3b
Lunawood <whail14@naver.com>
2022-10-06 23:54:00 +0900

    BaekJoon

--- 06.BaekJoon/BaekJoon/1620(poketmon).cpp
+++ 06.BaekJoon/BaekJoon/1620(poketmon).cpp
@@ -1,65 +1,45 @@
-#include <iostream>
-#include <algorithm>
-#include <vector>
-#include <string> 
-#include <sstream>
-#include <stdlib.h> 
-#include <queue>
+#include<iostream>
+#include<string>
+#include<map>
+#include<cctype>
 using namespace std;
 
-string InttoString[100001];
-vector<pair<string, int> > StringtoInt;
-
-// 이진 탐색 
-int binarySearch(string tmp, int s, int e){
-	int m = (s + e) / 2;
-	if(s > e) return 0;
-	else {
-		if(StringtoInt[m].first == tmp) {
-			return StringtoInt[m].second;
-		}
-		else if(StringtoInt[m].first < tmp) return binarySearch(tmp, m + 1, e);
-		else return binarySearch(tmp, s, m - 1);
-	}
-}
-
+// Value to Key 
+string poketmonNtoS[100001];
+// map 선언: key(string), value(int)
+map<string, int> poketmonStoN;
+  
 int main(){
+	// cin, cout 입출력 속도 가속
+	// 1. scanf, prinf를 섞어서 사용x
+	// 2. 싱글 쓰레드 환경에서만 사용할 것
+	// endl보다 \n을 사용하는 것이 빠르다. 
+	ios_base :: sync_with_stdio(false);
 	cin.tie(NULL);
-    ios_base::sync_with_stdio(false);	//없으면 시간 초과
-    
-	int N, M, tot = 0;
-	string tmp;
-	queue<string> q;
+	cout.tie(NULL);
+	
+	// N: 정보 입력 갯수, M: 문제 입력 갯수 
+	int N, M;
 	cin >> N >> M;
-	for(int i = 1; i < N + 1; i++){
-		cin >> tmp;
-		InttoString[i] = tmp;	// 숫자->문자, 배열 
-		StringtoInt.push_back(make_pair(tmp, i));	// 문자->숫자, vector 
-	}
 	
-	// 문자 정렬 : 이진 검색을 사용하기 위해서... 
-	sort(StringtoInt.begin(), StringtoInt.end());
+	string info;
+	for(int i = 0; i < N; i++){
+		cin >> info;
+		poketmonNtoS[i + 1] = info;
+		poketmonStoN.insert(make_pair(info, i + 1)); 
+	}
 
+	string problem;
 	for(int i = 0; i < M; i++){
-		cin >> tmp;
-		int n;
-		// 문자와 숫자 구분 
-		if((int)isdigit(tmp.at(0))) {	// 숫자 -> 글자 
-			n = atoi(&tmp.at(0));
-			q.push(InttoString[n]);
-		} else {	// 글자 -> 숫자 
-			stringstream ss;
-			ss << binarySearch(tmp, 0, N);
-			q.push(ss.str());
+		cin >> problem;
+		
+		if(0 < isdigit(problem[0]) and isdigit(problem[0]) < 10){
+			cout << poketmonNtoS[stoi(problem)] << "\n";
+		} else {
+			auto item = poketmonStoN.find(problem);
+			cout << item->second << "\n";
 		}
-	}
-	
-	// 출력 
-	while(q.size()){
-		cout << q.front() << endl;
-		q.pop();
 	}
 	
 	return 0;
 }
-
--- 06.BaekJoon/BaekJoon/Practice.cpp
+++ 06.BaekJoon/BaekJoon/Practice.cpp
@@ -1,1 +1,35 @@
+#include <iostream>
+#include <algorithm>
+#include <vector>
+using namespace std;
 
+int A[200001];
+
+int main(){
+	ios_base :: sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
+	
+	int N, M, num = 0;
+	cin >> N >> M;
+	
+	int a, b;
+	for(int i = 0; i < N; i++){
+		cin >> a;
+		A[a]++;
+	} 
+	for(int i = 0; i < M; i++){
+		cin >> b;
+		A[b]++;
+	} 	
+	
+	for(int i = 0; i < 200001; i++){
+		if(A[i] == 1){
+			num++;
+		}
+	}
+	
+	cout << num << endl;
+	
+	return 0;
+}
