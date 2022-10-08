7bd8c2d0a9cc49d1c1a01121f060e2bfc37eda7a
Lunawood <whail14@naver.com>
2022-10-08 23:14:24 +0900

    DijkstraAlgorithm

--- 19.DijkstraAlgorithm/DijkstraAlgorithm.cpp
+++ 19.DijkstraAlgorithm/DijkstraAlgorithm.cpp
@@ -4,6 +4,7 @@ int number = 6;
 int INF = 1000000000;
 
 // 전체 그래프를 초기화합니다.
+// 이어진 노드는 숫자로 표기, 이어지지 않은 노는 무한으로 표 
 int a[6][6] = {
 	{0, 2, 5, 1, INF, INF},
 	{2, 0, 3, 2, INF, INF},
@@ -22,23 +23,25 @@ int d[6];
 int getSmallIndex(){
 	int min = INF;
 	int index = 0;
-	// 선형 탐색 
+	// 선형 탐색  : 6개 모두 확인해본다. 
 	for(int i = 0; i < number; i++){
 		if(d[i] < min && !v[i]){
 			min = d[i];
 			index = i;
 		}
-	}
+	} 
 	return index;
 } 
 
 // 다익스트라를 수행하는 함수
 void dijkstra(int start){
+	// 각각의 노드와의 거리 입력 및 방문표시 
 	for(int i = 0; i < number; i++){
 		d[i] = a[start][i];
 	}
 	v[start] = true;
-	for(int i = 0; i< number - 2; i++){
+	
+	for(int i = 0; i < number - 2; i++){
 		int current = getSmallIndex();
 		v[current] = true;
 		for(int j = 0; j < 6; j++){
