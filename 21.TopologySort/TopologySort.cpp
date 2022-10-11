2fe265e69d271ebb99522ea69eaef17d46f97a61
Lunawood <whail14@naver.com>
2022-10-11 22:00:44 +0900

    TopologySort

---
+++ 21.TopologySort/TopologySort.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <vector>
+#include <queue>
+#define MAX 10
+using namespace std;
+
+int n, inDegree[MAX];
+vector<int> a[MAX];
+
+void topologySort(){
+	
+}
+
+int main(void){
+	
+	return 0;
+}
